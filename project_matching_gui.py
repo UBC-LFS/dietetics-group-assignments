00d61f4749f1e547210933feefc5c606a722d4f1
TracyGan <tracyygann@gmail.com>
2025-10-02 16:09:38 -0700

    Append non-empty values

--- project_matching_gui.py
+++ project_matching_gui.py
@@ -339,8 +339,15 @@ class ProjectMatchingGUI:
         for key, widget in self.user_inputs.items():
             if isinstance(widget, tk.BooleanVar):
                 collected_user_inputs[key] = widget.get()
+                
             elif isinstance(widget, list):
-                collected_user_inputs[key] = [(e1.get(), e2.get()) for e1, e2 in widget]
+                results = []
+                for a, b in widget:
+                    v1, v2 = a.get().strip(), b.get().strip()
+                    if v1 and v2:
+                        results.append((v1, v2))
+                collected_user_inputs[key] = results
+
             elif isinstance(widget, dict):
                 collected_user_inputs[key] = {
                     "min": widget["min"].get(),
@@ -349,16 +356,10 @@ class ProjectMatchingGUI:
             else:
                 collected_user_inputs[key] = widget.get()
         print(collected_user_inputs)
-        popup.destroy
+        popup.destroy()
 
 
 if __name__ == "__main__":
     root = tk.Tk()
     app = ProjectMatchingGUI(root)
     root.mainloop()
-
-# TODO Priorities:
-# 1. match the variables from the popup to the matching algorithm
-# 3. button to run the matching algorithm in scripts/script.py
-# 4. figure out where to save the csv file -> we will have 2 different ones, one for canvas group matching and one for profs
-# 5. (?) Optional for now: create another button to validate csv file before matching
