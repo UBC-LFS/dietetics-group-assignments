df220fe6a7dc059f16262826fd3d308ad52c664d
lattellie <lattellie@gmail.com>
2026-09-15 10:36:00 -0700

    accept raw data

--- upload_csv.py
+++ upload_csv.py
@@ -8,8 +8,19 @@ from PySide6.QtWidgets import (
     QVBoxLayout, QHBoxLayout, QWidget,
     QFileDialog, QComboBox, QPushButton, QLineEdit, QLabel 
 )
+import pandas as pd
+from enum import Enum
+import re
 
-
+class responseField(Enum):
+    NAME = 'student_name'
+    FIRST_NAME = 'student_first_name'
+    LAST_NAME = 'student_last_name'
+    EMAIL = 'student_email'
+    STUDENT_NUMBER = 'student_number'
+    PROJECT = 'project_column_index'
+    
+    
 class UploadCSVPage(QWidget):
     def __init__(self):
         super().__init__()
@@ -28,15 +39,83 @@ class UploadCSVPage(QWidget):
         self._setup_gui()
 
     
+    # output a dictionary of 'student_first_name': idx, etc.
     def extract_parameters(self):
         if self.csv_file_path == "" or self.csv_file_name == "":
             raise FieldError("No File Selected", "Please upload a csv file")
+        # for filtereing QX_Y to QX for later grouping
+        def get_q_number(col): 
+            match = re.match(r"^(Q\d+)(?:_\d+)?$", str(col))
+            return match.group(1) if match else None
+        # for removing the common question header from qualtrix
+        def common_prefix(strings): 
+            strings = [str(s) for s in strings]
+            if not strings:
+                return ""
+            if len(strings) == 1:
+                return ""
+            prefix = strings[0]
+            for s in strings[1:]:
+                while not s.startswith(prefix):
+                    prefix = prefix[:-1]
+                    if not prefix:
+                        return ""
+            return prefix
+        df = pd.read_csv(self.csv_file_path)
+        filtered = df.loc[:, df.columns.astype(str).str.match(r"^Q\d+(?:_\d+)?$")] # keeping only the actual question
+        for q, cols in filtered.T.groupby(filtered.columns.map(get_q_number)):
+            prefix = common_prefix(cols[0])
+            selected_col_name = cols.T.columns
+            filtered.loc[filtered.index[0], selected_col_name] = (
+                filtered.loc[filtered.index[0], selected_col_name]
+                .astype(str)
+                .str.removeprefix(prefix)
+            )
+        only_q_num = filtered.columns.map(get_q_number)
+        most_common = only_q_num.value_counts().idxmax()
+        first_index = only_q_num.get_loc(most_common).start
 
-        if self.selected_header is None:
-            raise FieldError("Select Header", "Please configure the parameters for the matching algorithm")
-
-        index_info = self.header_options[self.selected_header]["indices"] 
-        return self.csv_file_path, self.csv_file_name, index_info
+        filtered.columns = filtered.iloc[0]
+        filtered = filtered.iloc[2:].reset_index(drop=True)
+        col_names = filtered.columns
+                
+        current_id = 0
+        index_info = {}
+        student_id_index = None
+        for current_id in range(first_index):
+            if 'first' in col_names[current_id].lower():
+                index_info[responseField.FIRST_NAME] = current_id
+            elif 'last' in col_names[current_id].lower():
+                index_info[responseField.LAST_NAME] = current_id
+            elif 'name' in col_names[current_id].lower():
+                index_info[responseField.NAME] = current_id
+            elif 'mail' in col_names[current_id].lower():
+                index_info[responseField.EMAIL] = current_id
+            elif 'student id' in col_names[current_id].lower():
+                index_info[responseField.STUDENT_NUMBER] = current_id
+                student_id_index = current_id
+            elif 'student number' in col_names[current_id].lower():
+                index_info[responseField.STUDENT_NUMBER] = current_id
+                student_id_index = current_id
+        index_info[responseField.PROJECT] = first_index
+        if student_id_index is None:
+            def is_student_number_column(series, threshold=0.7):
+                values = series.dropna().astype(str).str.strip()
+                if len(values) == 0:
+                    return False
+                valid = values.str.fullmatch(r"\d{8,}")
+                return valid.mean() >= threshold
+                
+            for i, col in enumerate(filtered.columns):
+                if is_student_number_column(filtered[col]):
+                    student_id_index = i
+                    break
+        if student_id_index is None:
+            raise ValueError(f"Unable to find student number column")
+        filtered_index_info = {key.value: value for key, value in index_info.items()}
+        filtered = filtered.drop_duplicates(subset=filtered.columns[student_id_index], keep='last')
+        filtered.to_csv(self.csv_file_path.replace(".","_cleaned."), index=False)
+        return self.csv_file_path.replace(".","_cleaned."), self.csv_file_name.replace(".","_cleaned."), filtered_index_info
 
 
     def _setup_gui(self):
@@ -72,20 +151,20 @@ class UploadCSVPage(QWidget):
         file_display_layout.addWidget(file_label)
         file_display_layout.addWidget(self.file_entry, alignment=Qt.AlignLeft, stretch=1)
 
-        header_dropdown = QComboBox()
-        header_dropdown.addItem("Select the header which corresponds to the CSV file uploaded.") # TODO!!
-        index = header_dropdown.count() - 1
-        header_dropdown.model().item(index).setEnabled(False)
+        # header_dropdown = QComboBox()
+        # header_dropdown.addItem("Select the header which corresponds to the CSV file uploaded.") # TODO!!
+        # index = header_dropdown.count() - 1
+        # header_dropdown.model().item(index).setEnabled(False)
 
-        for key in self.header_options.keys():
-            header_dropdown.addItem(self.header_options[key]["header_values"], key)
+        # for key in self.header_options.keys():
+        #     header_dropdown.addItem(self.header_options[key]["header_values"], key)
 
-        layout.addWidget(header_dropdown, alignment=Qt.AlignLeft)
+        # layout.addWidget(header_dropdown, alignment=Qt.AlignLeft)
 
-        side_note = QLabel("Note: The header values in the dropdown have to correspond to the correct columns.")
-        side_note.setFont(QFont(MAIN_FONT, 12))
-        side_note.setWordWrap(True)
-        layout.addWidget(side_note)
+        # side_note = QLabel("Note: The header values in the dropdown have to correspond to the correct columns.")
+        # side_note.setFont(QFont(MAIN_FONT, 12))
+        # side_note.setWordWrap(True)
+        # layout.addWidget(side_note)
 
         self.configure_button = QPushButton("Configure Groups")
         
@@ -115,7 +194,7 @@ class UploadCSVPage(QWidget):
 
         # Setup signals
         upload_button.clicked.connect(self._upload_csv_file)
-        header_dropdown.currentIndexChanged.connect(self._update_selected_header)
+        # header_dropdown.currentIndexChanged.connect(self._update_selected_header)
     
 
     @Slot()
